b46902fdc5017e2bf56a827599e63e6dbcc5294b
Edwinliby <edwinliby30@gmail.com>
2022-06-14 18:42:18 +0530

    Bike App

---
+++ app/src/main/java/com/example/bike_app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.bike_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide(); // hide the title bar
+        setContentView(R.layout.activity_main);
+    }
+}
